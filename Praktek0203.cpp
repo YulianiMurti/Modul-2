e7d9987fc98695e66c5a53f6eb192d2ca7c52c08
YulianiMurti <yuliamurtii@gmail.com>
2017-10-23 16:22:07 +0800

    Menghitung dan menampilkan jumlah kotak dalam 1 ton

--- Praktek0203.cpp
+++ Praktek0203.cpp
@@ -5,10 +5,15 @@ int main(){
 	double ton=35273.92;
 	
 	double berat;
+	cout << "Masukan Berapa Berat Sereal dalam 1 Kotak Kemasan (satuan ounce) = ";
 	cin >> berat;
 	
 	double ounce = 1/ton;
 	double beratTon = berat*ounce;
+	cout << "1 Kotak Kemasan Sereal Beratnya (satuan Ton) = ";
 	
 	cout << beratTon<<endl;	
+	
+	double jumlah=1/beratTon;
+	cout <<"Jika Terdapat 1 Ton Sereal, Maka Akan memerlukan = "<< jumlah;cout<<"kotak";
 }
